60966389d600a0db2881caa43f6ab870b6d295ab
Gavin Barro <g.ab28320@gmail.com>
2026-01-07 17:41:44 -0500

    test file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,35 @@
+import java.util.*;
+
 public class HelloWorld {
 
-    public static void main(String[] args) {
-        String msg = "Hello World!";
-        System.out.println(msg);
+    private int count;
+
+    public HelloWorld() {
+        this.count = 1;
     }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public static void main(String[] args) {
+        String msg = "Hello";
+        String msg2 = " ";
+        String msg3 = "World!";
+        List<String> array = new ArrayList<>();
+        array.add(msg);
+        array.add(msg2);
+        array.add(msg3);
+
+        for (String str : array) {
+            System.out.print(str);
+        }
+        System.out.println();
+        System.out.println("Returning exit code: " + 0);
+    }
+
 }
