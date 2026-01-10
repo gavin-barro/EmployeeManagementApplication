bda099b26263a0c40a36841c5a4ebc1855e15c0a
Gavin Barro <g.ab28320@gmail.com>
2026-01-09 19:24:23 -0500

    set up and test backend and mysql

---
+++ springboot-backend/src/main/java/net/javaguides/springboot_backend/controller/EmployeeController.java
@@ -0,0 +1,27 @@
+package net.javaguides.springboot_backend.controller;
+
+import net.javaguides.springboot_backend.model.Employee;
+import net.javaguides.springboot_backend.repository.EmployeeRepository;
+import org.hibernate.service.spi.InjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    // get all employees
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+}
--- springboot-backend/src/main/java/net/javaguides/springboot_backend/exception/ResourceNotFound.java
+++ springboot-backend/src/main/java/net/javaguides/springboot_backend/exception/ResourceNotFound.java
@@ -1,4 +1,15 @@
 package net.javaguides.springboot_backend.exception;
 
-public class ResourceNotFound {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value= HttpStatus.NOT_FOUND)
+public class ResourceNotFound extends RuntimeException {
+
+    private static final long serialVerisonID = 1L;
+
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+
 }
